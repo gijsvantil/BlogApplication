290175657654da57e1dea96e1af4bbdda3e3d33b
Gijs van Til <gijsvantil@gmail.com>
2016-05-30 21:02:26 +0200

    added comments to app.js and linked index.pug to (bootstrap) stylesheets

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var Sequelize = require('sequelize');
+// REQUIRING Sequelize, Express and Body-parser
+pvar Sequelize = require('sequelize');
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var app = express();
+// static folder
+app.use(express.static('./resources/'));
 
+// Setting pug as view engine
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+// Simple get that listens on '/' and renders 'index'
 app.get('/', (req,res)=>{
 	res.render('index')
 });
